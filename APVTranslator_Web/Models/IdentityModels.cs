00adebbbfb8d32e432350235bda138b12e54e988
Tran Trong Tien DAT <Tran Trong Tien DAT@APV-PC050>
2016-11-01 15:03:05 +0700

    code login page, add bootstrap-social.css, font-awesome.css

--- APVTranslator_Web/Models/IdentityModels.cs
+++ APVTranslator_Web/Models/IdentityModels.cs
@@ -16,6 +16,7 @@ namespace APVTranslator_Web.Models
             // Add custom user claims here
             return userIdentity;
         }
+      
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
