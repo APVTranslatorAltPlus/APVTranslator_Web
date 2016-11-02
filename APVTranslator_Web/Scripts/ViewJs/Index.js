a9d35289119a1701705aa01f174a11d34cff2213
Tran Trong Tien DAT <Tran Trong Tien DAT@APV-PC050>
2016-11-02 15:09:59 +0700

    ger data from row click event in Dashboard

--- APVTranslator_Web/Scripts/ViewJs/Index.js
+++ APVTranslator_Web/Scripts/ViewJs/Index.js
@@ -1,16 +1,38 @@
-﻿var app = angular.module('myApp', ['ngGrid']).controller('MyCtrl', ['$scope', '$http', '$domUtilityService', function ($scope, $http, $domUtilityService) {
+﻿
+
+var app = angular.module('myApp', ['ngGrid']).controller('MyCtrl', ['$scope', '$http', '$domUtilityService', function ($scope, $http, $domUtilityService) {
     $scope.init = function (project) {
         debugger;
         $scope.data = project;
     }
     $scope.gridOptions = {
         data: 'data',
-        enableCellSelection: true,
-        enableRowSelection: false,
+        enableCellSelection: false,
+        enableRowSelection: true,
         enableCellEditOnFocus: false,
-        columnDefs: [{ field: 'Title', displayName: 'ProjectName', enableCellEdit: true, resizable: true },
-                     { field: 'Path', displayName: 'Path', enableCellEdit: true, resizable: true },
-                     { field: 'CreateBy', displayName: 'CreateBy', enableCellEdit: false, resizable: true }]
+        columnDefs: [{ field: 'Title', displayName: 'ProjectName',  resizable: true },
+                     { field: 'Path', displayName: 'Path',  resizable: true },
+                     { field: 'CreateBy', displayName: 'CreateBy', resizable: true }],
+        rowTemplate: '<div ng-click="onClickRow(row)" ng-style="{\'cursor\': row.cursor, \'z-index\': col.zIndex() }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell {{col.cellClass}}" ng-cell></div>'
+    };
+
+    $scope.onClickRow = function (rowItem) {
+       // alert(rowItem.entity.Id);
+        document.getElementById("ProjectName").innerHTML = rowItem.entity.Title
+        document.getElementById("Status").innerHTML = rowItem.entity.Status
+        document.getElementById("Progress").innerHTML = rowItem.entity.Progress
+        document.getElementById("Path").innerHTML = rowItem.entity.Path
+        document.getElementById("TranslateLanguage").innerHTML = rowItem.entity.TranslateLanguageID
+        document.getElementById("CreateAt").innerHTML = rowItem.entity.CreateAt
+        document.getElementById("CreateBy").innerHTML = rowItem.entity.CreateBy
+        document.getElementById("Deadline").innerHTML = rowItem.entity.DeadLine
+
+        if (rowItem.entity.Status === 'false') {
+
+        } else {
+            document.getElementById("Status").innerHTML = "Translating"
+        }
+
     };
 }])
 $(document).ready(function () {
