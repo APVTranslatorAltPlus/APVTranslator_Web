257ac5cbf9f4df7c10055e6b07fc3e239daa347a
Tran Trong Tien DAT <Tran Trong Tien DAT@APV-PC050>
2016-11-15 15:02:42 +0700

    revert and merge

---
+++ APVTranslator_Web/Scripts/layouts/_Layout.js
@@ -0,0 +1,9 @@
+﻿var apvApp = angular.module('apvApp', ['ngGrid', 'ngMaterial', 'chieffancypants.loadingBar', 'ngAnimate', 'ngTagsInput', 'ui.bootstrap.datetimepicker', 'ui.dateTimeInput'])
+    .config(function (cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.includeSpinner = true;
+    })
+    .controller('ctTopbar', ['$scope', '$http', 'cfpLoadingBar', function (scope, http, cfpLoadingBar) {
+        scope.test = function () {
+            alert('abc');
+        }
+    }])
